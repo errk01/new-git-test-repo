d7a52b345710d14b2ccb2b4377c23ac51036a5f0
errk01 <erik.taylor30@gmail.com>
2021-04-14 09:04:19 -0700

    javaScript intro

---
+++ week2/dayThree/script/scripts.js
@@ -0,0 +1,162 @@
+// // var, let, const
+
+// // Numbers
+// // let x
+// // x = 5;
+
+// // let h = 7.0009;
+
+// // // snake case
+// // const my_fave_number = 7
+
+// // let y = x % my_fave_number;
+
+// // Strings
+
+// let a = "hello ";
+// let b = "this is a string"
+// let c = "7";
+// let d = "it's seven";
+// // this is the esc
+// let f = 'it\'s seven'
+
+// // this is the new line
+// let g = "it's \n seven";
+
+// // boolean
+
+// let i = false
+
+// // tells you what it is
+// // console.log(typeof h);
+
+// // Implicit
+
+// let num = 7+7+7
+
+// let num2 = 5+5+' 5'
+// let num3 = Number('3'+3+3)
+// // console.log(num3)
+
+// let amount = 1000;
+
+// // let donation = prompt("How much would you like to give me");
+
+// // amount = Number(donation) + amount;
+
+// // console.log("what we have: "+ amount);
+
+// // type casting
+
+// let strnum = parseInt("10");
+ 
+// let newAmount = parseFloat("10.25");
+
+// let numStr = toString(5.99);
+
+
+// // console.log(typeof numStr)
+
+// // concatenation
+
+// let gretting = "Hello";
+
+// let fName = "Joe"
+
+// // console.log(gretting + fName)
+
+// // Template literals
+
+// // console.log(`${gretting} ${x} ${fName}, how are you?`)
+
+
+// let something;
+
+// // console.log(typeof something)
+
+// // Flow and control
+
+// let x = 10;
+// let seven = 7;
+
+// // console.log('seven', seven === '7')
+
+
+// // if(x === 5){
+// //     console.log(true);
+// // } 
+// // else if (x == 'true'){
+// //     console.log('this wont run');
+// // }
+// // else{
+// //     console.log(false);
+// // }
+
+
+// // so the && both have to be true
+// // the || only one needs to be true
+
+// // let temp = 90;
+
+// if(temp <= 70 && temp <=90){
+//     console.log("print")
+// } else {
+//     console.log("no print")
+// }
+
+
+// // for(let i = 1; i < 10; i++){
+// //     console.log('for loop', i);
+// // }
+
+// // for(let bottle = 10; bottle >= 0; bottle=bottle-1){
+// //     if(bottle === 0){
+// //         console.log("go buy more")
+// //     }else{
+// //         console.log(`${bottle} more to drink`)
+// //     }
+// // }
+
+
+// // start
+let n = 1;
+
+// end
+// while(n > 10){
+//     n++
+//     console.log(n)
+// }
+
+// do{
+//     n = n+1
+//     console.log(n)
+// }while(n > 10)
+
+switch(n){
+    case 3:
+        console.log('three')
+        break;
+    case 1: 
+        console.log('one')
+        break;
+    case 2:
+        console.log("two")
+        break;
+    default:
+        console.log('default')
+        break;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
